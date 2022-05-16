53d51962a1e62a6ce490be43d24d687bac181983
noah-schechter <noah.schechter1@gmail.com>
2022-05-15 22:58:02 -0700

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  darkMode: 'class',
+  content: ["/public/index.html"],
+  theme: {
+    extend: {colors: {
+      brown: '#2E2111',//from a darker shade of mushroom https://mushroomcoloratlas.com/mushroom/pisolithus_tinctorius
+      white: '#FFF',
+      green: '#585a33' //taken from mushroom https://mushroomcoloratlas.com/mushroom/polyozellus_atroazulinus 
+    },
+    fontFamily: {
+      iosevka: ["IOSEVKA", "sans-serif"]
+    }
+    }
+  },
+  plugins: [],
+}
